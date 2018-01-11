31cda3854cc4a8c8891f885606b4993720bac8e5
Fabrício Liberato <fabricio.liber@gmail.com>
2018-01-11 20:56:40 -0300

    Utilizando arrow funtion e tags semanticas

--- js/script.js
+++ js/script.js
@@ -1,42 +1,36 @@
-Array.prototype.uniq = function () {	
+Array.prototype.uniq = function () {
   	let uniq = []
   	let values = this
   	values.forEach(function (v) {
-		if (!uniq.includes(v)) 
+		if (!uniq.includes(v))
 		uniq.push(v)
 	})
   	return uniq
 }
-  
-  
 
-genero = document.querySelector('#generoMusical')
-artist = document.querySelector('#art-banda')
-numPer = document.querySelector('#numPer')
+var genero = document.querySelector('#generoMusical')
+var artist = document.querySelector('#art-banda')
+var numPer = document.querySelector('#numPer')
 
 //evento para receber o valor da escolha do genero musical
-genero.addEventListener('change', function(){
-	let resp_gen = genero.value
-	achaArt(resp_gen)
-})
+genero.addEventListener('change', gen => achaArt(genero.value))
 
 //função para mandar a resposta anterior para pegar artistas daquele genero no json do vagalume via url
 function achaArt(resp_gen){
 	let url = `https://www.vagalume.com.br/browse/style/${resp_gen}.js`
-	
+
+  const itemArt = i => `<option value="${i.artUrl}">${i.artDesc}</option>`
+  const result = (item) => {
+    artist.innerHTML = item.playlist.map(itemArt).sort().uniq().join('')
+  }
 	fetch(url)
-	.then(resposta => resposta.json()) //.then é equivalente ao sucess, o primeiro recebe a resposta e extrai apenas o json útil dela
-	.then( function(data){ //aqui vai oq vc faz com a resposta definitiva
-		let art = data.playlist.map(item=> `<option value="${item.artUrl}">${item.artDesc}</option>`).sort().uniq().join('')
-		artist.innerHTML = art
-  })	
+	         .then(resposta => resposta.json()) //.then é equivalente ao sucess, o primeiro recebe a resposta e extrai apenas o json útil dela
+	         .then(result) //aqui vai oq vc faz com a resposta definitiva
 }
 
 let qnt
 //evento para pegar a qnt de questoes a ser respondida
-numPer.addEventListener('change', function(){
-	qnt = numPer.value
-})
+numPer.addEventListener('change', num => qtd = num.value)
 
 //função para gerar perguntas
 //função para gerar o quiz
