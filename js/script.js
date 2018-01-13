0b39990166805f2d10c1d95bc7f1e4b35d80b816
Fabrício Liberato <fabricio.liber@gmail.com>
2018-01-12 22:05:41 -0300

    Criando array com musicas de um art

--- js/script.js
+++ js/script.js
@@ -17,6 +17,13 @@ var genero = document.querySelector('#generoMusical')
 var artist = document.querySelector('#art-banda')
 var numPer = document.querySelector('#numPer')
 var questions = document.querySelector('.perguntas')
+var botaoIniciar = document.querySelector('.botao')
+// Variavel pra pegar o nome das musicas e artistas da funcao achaArt
+var artistas = {}
+// Variavel para receber a filtragem do json 'artistas', contendo apenas msc de um artista
+var artistaFiltro = []
+// Chave do vagalume
+var key = 'c3f6644637dc1802b86c528e33ba0f78'
 
 //evento para receber o valor da escolha do genero musical
 genero.addEventListener('change', () => achaArt(genero.value))
@@ -27,6 +34,7 @@ function achaArt(resp_gen){
 
   const itemArt = i => `<option value="${i.artUrl}">${i.artDesc}</option>`
   const result = (item) => {
+		artistas = item.playlist
     artist.innerHTML = item.playlist.map(itemArt).sort().uniq().join('')
   }
   fetch(url)
@@ -34,22 +42,49 @@ function achaArt(resp_gen){
 	         .then(result) //aqui vai oq vc faz com a resposta definitiva
 }
 
-//evento para pegar a qnt de questoes a ser respondida
-numPer.addEventListener('change', () => {
-  geraPerguntas(perguntas, numPer.value)
+// Utilizo pra executar a filtragem de musicas por artistas e usá-las nas perguntas
+artist.addEventListener('click', () =>{
+	teste = []
+	for (i = 0; i < artistas.length; i++) {
+		if(artist.value === artistas[i].artUrl)
+			teste.push(artistas[i])
+	}
+	artistaFiltro = teste
+	console.log(artistaFiltro[0].musDesc)
 })
 
-//Função para gerar a selecao aleatória das perguntas
+// Botao utilizado para simular o inicio do jogo, onde abrira o pop-up para iniciar as perguntas
+botaoIniciar.addEventListener('click', () => {
+	geraPerguntas(perguntas, numPer.value, artistaFiltro)
+})
+
+/*evento para pegar a qnt de questoes a ser respondida (Achei redundante)
+numPer.addEventListener('change', () => {
+  console.log(numPer.value)
+	geraPerguntas(perguntas, numPer.value)
+})*/
+
+/*Função para gerar a selecao aleatória das perguntas
+(Funcao floor faz com que o numero seja arredondado pra baixo)*/
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
 //função para gerar perguntas
-function geraPerguntas (perguntas, qtd) {
+function geraPerguntas (perguntas, qtd, art) {
+	//Aqui utilizei o valor [0] para agilizar o processo, mas isso tbm vira de forma 'ramdomica'
+	url = `https://api.vagalume.com.br/search.php?art=${art[0].artUrl}&mus=${art[0].musDesc}&apikey=${key}`
+	console.log(url)
+	const result = (item) => {
+    console.log(item.mus[0].text)
+  }
+	fetch(url)
+					 .then(resposta => resposta.json()) //.then é equivalente ao sucess, o primeiro recebe a resposta e extrai apenas o json útil dela
+					 .then(result)
   questions.innerHTML = ''
   for (i = 0; i < qtd; i++)
     questions.innerHTML += `<h2>${perguntas[getRandomInt(0, perguntas.length)]}</h2>`
 }
-//console.log(geraPerguntas(perguntas));
 
 
 //função para gerar o quiz
