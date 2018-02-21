e39c7c846ebafcd8a4022f2715b7c2b0f247e37d
Rafael Lins <rlinsdesouza@gmail.com>
2018-02-21 18:17:31 -0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         use: [
           {
             loader: 'file-loader',
-            options: {name: '[name].[ext]', outputPath: 'img/', publicPath: '../' }
+            options: {name: '[name].[ext]', outputPath: 'img/', publicPath: '../img/' }
           }
         ]
       },
